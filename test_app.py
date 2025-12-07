aa2198cc40fe57d076a4ac1808117fa486b279fb
HanaaMAldaly <hanaaaldaly@gmail.com>
2025-12-07 19:06:15 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,6 @@ def client():
     return app.test_client()
 
 def test_metrics_endpoint(client):
-    """تأكد إن /metrics بيرجع 200"""
     response = client.get("/metrics")
     assert response.status_code == 200
 
